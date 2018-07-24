e7b914813f95b8b99687930bbea6a07965df23f1
at15 <at15@outlook.com>
2018-07-23 20:59:28 -0700

    [cli] Use chzyer/readline for better auto complete
    - remove github.com/peterh/liner it does not show all the complete
    options, can only use tab to switch between options

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+VERSION = 0.0.1
+BUILD_COMMIT = $(shell git rev-parse HEAD)
+BUILD_TIME = $(shell date +%Y-%m-%dT%H:%M:%S%z)
+CURRENT_USER = $(USER)
+FLAGS = -X main.version=$(VERSION) -X main.commit=$(BUILD_COMMIT) -X main.buildTime=$(BUILD_TIME) -X main.buildUser=$(CURRENT_USER)
+
+# --- cassandra ---
 .PHONY: run-c2 run-c3 shell-c2 shell-c3 down
 run-c2:
 	docker-compose up c2
@@ -9,10 +16,18 @@ shell-c3:
 	docker-compose exec c3 /bin/bash
 down:
 	docker-compose down
+# --- cassandra ---
 
+.PHONY: fmt install dep-update dep-install
 fmt:
 	gofmt -d -l -w ./cmd ./pkg
 
 install:
-	go install ./cmd/gocqlsh
-	go install ./cmd/goyourcassandra
+	go install -ldflags "$(FLAGS)" ./cmd/gocqlsh
+	go install -ldflags "$(FLAGS)" ./cmd/goyourcassandra
+
+dep-update:
+	dep ensure -v -update
+
+dep-install:
+	dep ensure
--- cmd/gocqlsh/main.go
+++ cmd/gocqlsh/main.go
@@ -2,45 +2,76 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"strings"
 
+	"github.com/chzyer/readline"
 	"github.com/gocql/gocql"
-	"github.com/peterh/liner"
 )
 
+var connectOnStart bool
+
+var _ readline.AutoCompleter = (*cqlshCompleter)(nil)
+
+type cqlshCompleter struct {
+}
+
+// Readline will pass the whole line and current offset to it
+// Completer need to pass all the candidates, and how long they shared the same characters in line
+// Example:
+//   [go, git, git-shell, grep]
+//   Do("g", 1) => ["o", "it", "it-shell", "rep"], 1
+//   Do("gi", 2) => ["t", "t-shell"], 2
+//   Do("git", 3) => ["", "-shell"], 3
+func (c *cqlshCompleter) Do(line []rune, pos int) (newLine [][]rune, length int) {
+	fmt.Println("line is", string(line))
+	return [][]rune{
+		{'o'},
+		{'i', 't'},
+	}, len(line)
+}
+
 func main() {
-	// TODO: print version etc.
-	fmt.Println("gocqlsh version 0.1")
-	cluster := gocql.NewCluster("localhost")
-	cluster.Keyspace = "system"
-	session, err := cluster.CreateSession()
+	fmt.Printf("gocqlsh version %s\n", version)
+	if connectOnStart {
+		cluster := gocql.NewCluster("localhost")
+		cluster.Keyspace = "system"
+		session, err := cluster.CreateSession()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer session.Close()
+	}
+
+	completer := cqlshCompleter{}
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:       ">",
+		HistoryFile:  "/tmp/readline.tmp",
+		AutoComplete: &completer,
+	})
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	defer session.Close()
-	line := liner.NewLiner()
-	defer line.Close()
-	line.SetMultiLineMode(true)
-	line.SetCompleter(func(line string) []string {
-		if strings.HasSuffix(line, "a") {
-			return []string{"apple", "add", "ageis"}
-		}
-		return nil
-	})
-	// TODO: history
 	for {
-		select {
-		// TODO: handle signals, though I think liner is also handling ctrl d etc.
-		default:
-			// TODO: should have username in the prompt
-			l, err := line.Prompt("cqlsh> ")
-			if err != nil {
-				log.Fatal(err)
-				return
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
 			}
-			fmt.Printf("you typed %s\n", l)
+		} else if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+		switch line {
+		case ":help":
+			fmt.Println("there is no help")
+		default:
+			fmt.Println("unknown", line)
 		}
 	}
 }
