f3b7e34f40765b00a68a537d30d18d74fdfb02a0
at15 <at15@outlook.com>
2018-07-24 19:20:30 -0700

    [ui] Add dev server proxy
    - NOTE: when proxy, the prefix is not trimmed by default, i.e. if you
    use /api/ping in browser and /api in proxy config, what reach the
    upstream is /api/ping, NOT /ping ....
    - init backend server, use gommon for logging

--- cmd/goyourcassandra/main.go
+++ cmd/goyourcassandra/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gocql/gocql"
+	dlog "github.com/dyweb/gommon/log"
+)
+
+var log = dlog.NewApplicationLogger()
+
+type connPool struct {
+	mu sync.Mutex
+
+	conns map[string]*gocql.Session
+}
 
 func main() {
-	fmt.Println("Hello, Cassandra")
+	fmt.Println("start http server")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("pong"))
+	})
+	mux.HandleFunc("/api/ping", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("api pong"))
+	})
+	logged := http.HandlerFunc(func(res http.ResponseWriter, r *http.Request) {
+		tw := &TrackedWriter{w: res, status: 200}
+		mux.ServeHTTP(tw, r)
+		log.InfoF("http", dlog.Fields{
+			dlog.Str("remote", r.RemoteAddr),
+			dlog.Str("proto", r.Proto),
+			dlog.Str("url", r.URL.String()),
+			dlog.Int("size", tw.Size()),
+			dlog.Int("status", tw.Status()),
+		})
+	})
+	http.ListenAndServe(":8080", logged)
 }
